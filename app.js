64189a0ba18c7e1f4ebb4a2b871588075b6d2074
swapnilbiraris92 <swapnil.biraris@a2iot.com>
2019-07-11 00:55:36 +0530

    assignment1

--- app.js
+++ app.js
@@ -3,8 +3,8 @@
     angular.module('assignment1', [])
     .controller('assignment1Controller', function ($scope) {
         $scope.lunch_menu = 'list comma separated dishes you usually have for lunch'
-        $scope.message = ""
-        $.scope.check = function () {
+        //$scope.message = ""
+        $scope.check = function () {
             $scope.message = lunch_fun()            
         }
         let lunch_fun = function () {
