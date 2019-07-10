bd8db0802b2c633f0410c35b81a7868e1618a309
swapnilbiraris92 <swapnil.biraris@a2iot.com>
2019-07-11 00:54:35 +0530

    assignment1

--- app.js
+++ app.js
@@ -3,11 +3,12 @@
     angular.module('assignment1', [])
     .controller('assignment1Controller', function ($scope) {
         $scope.lunch_menu = 'list comma separated dishes you usually have for lunch'
+        $scope.message = ""
         $.scope.check = function () {
             $scope.message = lunch_fun()            
         }
         let lunch_fun = function () {
-            list_lunch_menu = $scope.lunch_menu.split(",")
+            let list_lunch_menu = $scope.lunch_menu.split(",")
             if (list_lunch_menu.length <= 3){
                 return "Enjoy!"
             } else {
