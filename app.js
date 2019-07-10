5f1a3f7015ec730b74cd2a8415851ab76ac86eb0
swapnilbiraris92 <swapnil.biraris@a2iot.com>
2019-07-11 00:27:59 +0530

    assignment1

---
+++ app.js
@@ -0,0 +1,7 @@
+(function () {
+    'use strict'
+    angular.module('assignment1', [])
+    .controller('assignment1Controller', function ($scope) {
+        $scope.lunch_menu = 'list comma separated dishes you usually have for lunch'
+    })
+})()
